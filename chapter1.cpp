a18ab175fae1c342872c21bba98b74ff40f980fa
Hardikk-glitch <hardikkchahar@gmail.com>
2024-11-06 11:14:46 +0530

    Add files via upload

---
+++ chapter1.cpp
@@ -0,0 +1,135 @@
+// Question 1
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Maths = 90\nPhysics = 77\nChemistry = 69\n";
+
+    return 0;
+
+}
+
+*/
+
+// Question 2
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float num1,num2;
+
+    cout << "Enter first number\n";
+
+    cin >> num1;
+
+    cout << "enter second number\n";
+
+    cin >> num2;
+
+
+    if (num1>num2)
+    {
+        cout << "The larger number is " << num1 << "\n";
+    }
+    else {
+        cout << "The larger number is " << num2 << "\n";
+    }
+
+}
+
+*/
+
+// Question 3
+
+/*
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float num1;
+
+    cin >> num1;
+
+    for (int i = 0; i < num1; i++)
+    {
+        cout << "Well done\n";
+    }
+
+
+}
+
+*/
+
+// Question 4
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float a,b,c,operation;
+
+    cout << "Enter a:\n";
+
+    cin >> a;
+
+    cout << "Enter b:\n";
+
+    cin >> b;
+
+    cout << "Enter c:\n";
+
+    cin >> c;
+
+
+    operation = a/(b-c);
+
+    cout << "The answer is " << operation << "\n";
+}
+
+Answer for part 1 is 4.16667
+
+Answer for part 2 is infinite
+
+*/
+
+// Question 5
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float fahrenheit,celsius;
+
+    cout << "Enter the temprature in Farehite to get the temprature in Celsius \n";
+
+    cin >> fahrenheit;
+
+    celsius =  5*(fahrenheit-32)/9;
+
+    cout << "The value of temprature is " << celsius << "degree celsius\n";
+    
+    }
+    
+*/
+
